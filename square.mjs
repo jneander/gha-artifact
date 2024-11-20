ddacba60abde628a04132ca2832cc78b64e55463
Jeremy Neander <jeremy.neander@gmail.com>
2024-11-19 21:37:54 -0600

    WIP

---
+++ square.mjs
@@ -0,0 +1,27 @@
+import fs from 'node:fs/promises'
+import {join} from 'path'
+import {fileURLToPath} from 'url'
+
+const dirname = fileURLToPath(new URL('.', import.meta.url))
+const dataFilePath = join(dirname, 'math-result.json')
+
+async function readData() {
+  try {
+    const result = await fs.stat(dataFilePath)
+    return JSON.parse(result)
+  } catch {
+    return {value: 2}
+  }
+}
+
+async function writeData(data) {
+  await fs.writeFile(dataFilePath, JSON.stringify(data))
+}
+
+async function run() {
+  const data = await readData()
+  data.value = data.value ** 2
+  await writeData(data)
+}
+
+void run()
