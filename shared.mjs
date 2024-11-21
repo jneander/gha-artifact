faf05d944156c77dee00b870dcd6e957cec560e6
Jeremy Neander <jeremy.neander@gmail.com>
2024-11-20 20:04:22 -0600

    extract reusable workflows

---
+++ shared.mjs
@@ -0,0 +1,20 @@
+import fs from 'node:fs/promises'
+import {join} from 'path'
+import {fileURLToPath} from 'url'
+
+const dirname = fileURLToPath(new URL('.', import.meta.url))
+const dataFilePath = join(dirname, 'example-data.json')
+
+export async function readData() {
+  try {
+    const result = await fs.readFile(dataFilePath)
+    return JSON.parse(result)
+  } catch {
+    console.debug('Data file does not exist.')
+    return {}
+  }
+}
+
+export async function writeData(data) {
+  await fs.writeFile(dataFilePath, JSON.stringify(data))
+}
--- square.mjs
+++ square.mjs
@@ -1,33 +1,14 @@
-import fs from 'node:fs/promises'
-import {join} from 'path'
-import {fileURLToPath} from 'url'
-
-const dirname = fileURLToPath(new URL('.', import.meta.url))
-const dataFilePath = join(dirname, 'math-result.json')
-
-async function readData() {
-  try {
-    const result = await fs.readFile(dataFilePath)
-    return JSON.parse(result)
-  } catch (e) {
-    console.error(e)
-    console.log('using default value')
-    return {value: 2}
-  }
-}
-
-async function writeData(data) {
-  await fs.writeFile(dataFilePath, JSON.stringify(data))
-}
+import {readData, writeData} from './shared.mjs'
 
 async function run() {
-  const entries = await fs.readdir(dirname)
-  for (const entry of entries) {
-    console.log(`local file: ${entry}`)
-  }
   const data = await readData()
-  console.log('current data is', data)
-  data.value = data.value ** 2
+
+  if (data.square == null || data.square > 1000000) {
+    data.square = 2
+  } else {
+    data.square = data.square ** 2
+  }
+
   await writeData(data)
 }
 
